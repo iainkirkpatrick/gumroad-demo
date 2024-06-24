aea37ebb16b4cf35a0829de40c8627f15a03fe49
iainkirkpatrick <iain@iainkirkpatrick.com>
2024-06-24 14:17:53 +1000

    adjust seed

--- lib/seed_helper.rb
+++ lib/seed_helper.rb
@@ -1,18 +1,18 @@
 module SeedHelper
   def self.create_products_and_variants
     # create a product of each kind
-    Product.create!(native_type: "digital", name: "Digital Product", price_range: '10', is_published: true)
+    Product.create!(native_type: "digital", name: "Digital Product", price_range: '10', rich_content: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Content for this digital product."}]}]}', is_published: true)
     
-    coffee = Product.create!(native_type: "coffee", name: "Coffee Product", thanks_message: 'Thanks for supporting my work!', is_published: true)
+    coffee = Product.create!(native_type: "coffee", name: "Coffee Product", description: 'This is a coffee product.', rich_content: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Thank you for supporting my work!"}]}]}', thanks_message: 'Thanks for supporting my work!', is_published: true)
     
-    commissions = Product.create!(native_type: "commissions", name: "Commissions Product", price_range: '500', is_published: true)
+    commissions = Product.create!(native_type: "commissions", name: "Commissions Product", description: 'This is a commissions product.', rich_content: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Get in touch at commissions@iainkirkpatrick.com"}]}]}', price_range: '500', is_published: true)
     
-    Product.create!(native_type: "calls", name: "Calls Product", price_range: '200', call_link: 'https://calendly.com/iain-oxlc/test-gumroad-meeting', is_published: true)
+    Product.create!(native_type: "calls", name: "Calls Product", description: 'This is a calls product.', price_range: '200', rich_content: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Please book a meeting using the calendar above!"}]}]}', call_link: 'https://calendly.com/iain-oxlc/test-gumroad-meeting', is_published: true)
     
     coffee.variants.create!([
-      { product_id: coffee.id, public_id: 'cof001', name: "Coffee", price: 5 },
-      { product_id: coffee.id, public_id: 'cof002', name: "Cocktail", price: 10 },
-      { product_id: coffee.id, public_id: 'cof003', name: "Pizza", price: 15 }
+      { product_id: coffee.id, public_id: 'cof001', name: "Coffee", price: 5, description: 'This is a coffee variant.' },
+      { product_id: coffee.id, public_id: 'cof002', name: "Cocktail", price: 10, description: 'This is a cocktail variant.' },
+      { product_id: coffee.id, public_id: 'cof003', name: "Pizza", price: 15, description: 'This is a pizza variant.' }
     ])
     
     commissions.variants.create!([
